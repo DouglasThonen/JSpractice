9b078d6fac5bf3870d1796cf384a3f67a73dff26
Douglas Thonen <dthonen@gmail.com>
2024-02-06 14:44:20 -0500

    js practice

--- Javascriptpractice.js
+++ Javascriptpractice.js
@@ -32,7 +32,50 @@
 
 // console.log(formatDate());
 
-const addNew = (str) =>
-str.indexOf('New!') === 0 ? str: `New! ${str}`;
+// const addNew = (str) =>
+// str.indexOf('New!') === 0 ? str: `New! ${str}`;
 
-console.log(addNew('New! Offers'));
+// console.log(addNew('New! Offers'));
+
+// const makeNewString = (str) => 
+// str.length < 3 ? str : str.slice(0, 3) + str.slice(-3);
+
+// console.log(makeNewString('abc'));
+// console.log(makeNewString('abcdef'));
+// console.log(makeNewString('abc123abc123'));
+
+// console.log(makeNewString('ab'));
+
+// const firstHalf = (str) => str.slice(0, str.length / 2);
+
+// console.log(firstHalf('temp'));
+// console.log(firstHalf('temple'));
+// console.log(firstHalf('temples'));
+
+// const concatenate = (str1, str2) => 
+// str1.slice(1) + str2.slice(1);
+
+// console.log(concatenate('abc', 'def'));
+// console.log(concatenate('junior', 'developer'));
+
+// const hundred = (num1, num2); {
+// if ((100 - num1) > (100 - num2));
+// return num2}
+
+// const closestTo100 = (a,b) => (100 - a) < (100 - b)? a : b ;
+
+// console.log(closestTo100(99, 1));
+// console.log(closestTo100(49, 51));
+// console.log(closestTo100(50, 50));
+
+const countChars = (str, char) =>
+str.split('').filter(ch => ch === char).length;
+
+const contains2To4 = (str,char) =>
+countChars(str, char) >= 2 && countChars(str, char) <= 4;
+
+console.log(contains2To4('ooh!', 'o'));
+console.log(contains2To4('oh!', 'o'));
+console.log(contains2To4('oooh!', 'o'));
+console.log(contains2To4('ooooh!', 'o'));
+console.log(contains2To4('oooooh!', 'o'));
